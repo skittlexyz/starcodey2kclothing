3b2e2c9b1f10edd29a12d620b23d27239c91664d
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2023-05-05 17:20:05 -0300

    Add files via upload

--- _js/outfitSwap.js
+++ _js/outfitSwap.js
@@ -1,4 +1,10 @@
 import outfits from "../assets/outfits/structure.json" assert { type: "json" };
-const element = document.querySelector('.main-content.subdivider > .subdivider.right > .cloth');
-const randomIndex = Math.floor(Math.random() * 15);
-element.setAttribute('src', outfits[randomIndex]);
+
+const elementDefault = document.querySelector('.main-content.subdivider > .subdivider.right > .clothDefault');
+const randomIndexDefault = Math.floor(Math.random() * 15);
+elementDefault.setAttribute('src', outfits[randomIndexDefault]);
+
+
+const elementMobile = document.querySelector('#mobile > .header > .clothMobile');
+const randomIndexMobile = Math.floor(Math.random() * 15);
+elementMobile.setAttribute('src', outfits[randomIndexMobile]);
